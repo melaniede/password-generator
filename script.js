c54a433647874c3b38ca402a33ccdfc37b145844
melaniede <debarrosmelanie@gmail.com>
2020-07-12 16:53:12 -0400

    password length validation

--- script.js
+++ script.js
@@ -11,4 +11,29 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+
+
+generateBtn.addEventListener("click", passwordLengthValidation)
+let passwordLength;
+
+// Password Length Prompt and Validation
+function passwordLengthValidation() {
+ passwordLength = parseInt(prompt("How long would you like your password to be?"));
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(prompt("Choose a length of at least 8 characters and no more than 128 characters"));
+  }
+    passwordCharactersValidation();    
+}
+
+//const passwordCharacters = prompt("What character types would you like to include?");
+
+
+// Ask user about lowercase, uppercase, numeric, and special characters
+function passwordCharactersValidation() {
+  let chooseLowercase = confirm("Would you like to include lowercase characters?");
+  let chooseUppercase = confirm("Would you like to include uppercase characters?");
+  let chooseNumeric = confirm("Would you like to include numeric values?");
+  let chooseSpecialCharacters = confirm("Would you like to include special characters?");
+}
+// Create radio buttons for lowercase, uppercase, numeric, and/or special characters
